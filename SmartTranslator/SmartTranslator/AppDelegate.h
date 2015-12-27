dcccb0054aaa9e3a86f1930a83deb2933dfac6c1
TNKHANH <ADMIN@TNKHANHs-iMac.local>
2015-12-27 13:40:01 +0700

    them file

---
+++ SmartTranslator/SmartTranslator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SmartTranslator
+//
+//  Created by TNKHANH on 12/27/15.
+//  Copyright © 2015 TNKHANH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
